0536cd04ed9767f846c02a0710219bb3c34770f3
MoitreyoGhosh <moitreyoghosh@gmail.com>
2025-02-18 22:21:36 +0530

    fix(subscription): Resolve bugs in subscription model

--- src/models/subscription.js
+++ src/models/subscription.js
@@ -2,11 +2,11 @@ import mongoose, { Schema } from "mongoose";
 
 const subscriptionSchema = new Schema({
     subscriper: {
-        type: Schema.Types.ObjectId // one who is subscribing
+        type: Schema.Types.ObjectId, // one who is subscribing
         ref: "User"
     },
     channel: {
-        type: Schema.Types.ObjectId //one to whom the 'subscriber' is subscribing
+        type: Schema.Types.ObjectId, //one to whom the 'subscriber' is subscribing
         ref: "User"
     }
 },{timestamps: true});
