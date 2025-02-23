c4dc861b7fdd5ccb780d6cd8bcb7a2a915fe820c
MoitreyoGhosh <moitreyoghosh@gmail.com>
2025-02-23 22:46:27 +0530

    feat(user): Add user channel profile retrieval and watch history functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -357,8 +357,134 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   ).select("-password");
 
   return res
-    .status(200)            
-    .json(new ApiResponse(200, user, "Cover image updated successfully"));            
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover image updated successfully"));
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing");
+  }
+  const channel = await User.aggregate([
+    {
+      //match is used to filter data 
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      //lookup is used to get data from other collection based on some condition
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: { 
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      //addFields is used to add new fields to the document
+      $addFields: {
+        subscribersCount: {
+          $size: $subscribers,
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed:{
+          $cond:{
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      },
+    },
+    {
+      //project is used to select fields to return
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+        //createdAt: 1
+      }
+    }
+  ]);
+
+  if(!channel?.length){
+    throw new ApiError(404, "Channel does not exist");
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, channel[0], "User Channel fetched successfully"));
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user =await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  }
+                },
+                {
+                  $addFields:{
+                    owner: {
+                      $first: "$owner"
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory, 
+      "Watch history fetched successfully"
+    )
+  );
 });
 
 export {
@@ -371,4 +497,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 };
